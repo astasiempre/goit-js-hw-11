3b690fd9a74551c7b970681a920542618e86220c
astasiempre <ero1494651@gmail.com>
2023-08-20 17:26:06 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
+const loadMore = document.querySelector('.load-more');
+const lightbox = new SimpleLightbox('.photo-link'); 
+
+
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '38912388-dfab1f4f09b0fb6a50a23584e';
+let searchQuery = '';
+
+loadMore.style.display = 'none'; 
 
 form.addEventListener('submit', async e => {
   e.preventDefault();
   console.log(e);
-  const searchQuery = e.target.elements.searchQuery.value;
-  // Виклик функції для виконання запиту до API з використанням Axios
-  
+  searchQuery = e.target.elements.searchQuery.value;
+
   const getImage = async function (searchQuery) {
     try {
-      const URL = 'https://pixabay.com/api/';
-      // const URL = 'https://pixabay.com/api/?key=38912388-dfab1f4f09b0fb6a50a23584e&q=dog&image_type=photo';
-      const API_KEY = '38912388-dfab1f4f09b0fb6a50a23584e';
       const params = new URLSearchParams({
         key: API_KEY,
-        q: 'dog',
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
@@ -25,48 +32,33 @@ form.addEventListener('submit', async e => {
         per_page: 40,
       });
       const response = await axios.get(`${URL}?${params}`);
-        const data = response.data;
-        console.log(data);
-    gallery.innerHTML = createMarkup(data.hits);
+      const data = response.data;
+      console.log(data);
+      Notiflix.Notify.info(
+        `Hooray! We found ${ data.totalHits } images.`
+      );
+      gallery.innerHTML = createMarkup(data.hits);
+      if (data.hits.length === 40) {
+        loadMore.style.display = 'block';
+      }
+      lightbox.refresh(); 
       
-    //   const { data } = await axios.get(`${URL}?${params}`);
-      // const data = resp.json();
-      
-      // console.log(resp)
-      // return data;
-    //   gallery.innerHTML = createMarkUp(data);
     } catch (error) {
       console.log(error);
     }
   };
- getImage(searchQuery);
+  getImage(searchQuery);
+});
 
-//   function createMarkUp(images) {
-//     return images
-//       .map(
-//         ({
-//           hits: { webformatURL, comments, likes, views, tags }
-//         }) => `<div class="photo-card">
-//   <img src="${webformatURL}" alt="${tags}" loading="lazy"  />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes: ${likes}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views: ${views}</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments: ${comments}</b>`
-//       )
-//           .join('');
-      
-//     }
-    function createMarkup(images) {
+function createMarkup(images) {
   return images
     .map(
-      ({ webformatURL, comments, likes, views, tags }) => `
+      ({ webformatURL, comments, likes, views, tags, largeImageURL
+ }) => `
+      <a href="${largeImageURL
+}" class="photo-link">
       <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" width=300/>
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width=300 height=200/>
         <div class="info">
           <p class="info-item">
             <b>Likes: ${likes}</b>
@@ -79,9 +71,39 @@ form.addEventListener('submit', async e => {
           </p>
         </div>
       </div>
+      </a>
     `
     )
     .join('');
 }
 
-});
+let currentPage = 9;
+loadMore.addEventListener('click', onLoadMore);
+async function onLoadMore() {
+  currentPage += 1;
+  try {
+    const params = new URLSearchParams({
+      key: API_KEY,
+      q: searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: 40,
+    });
+    const response = await axios.get(`${URL}?${params}`);
+    const data = response.data;
+    console.log(data);
+    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+    if (data.totalHits <= currentPage * 40) {
+      loadMore.style.display = 'none'; 
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (error) {
+    console.error('Error fetching more images:', error);
+  }
+}
+
+
